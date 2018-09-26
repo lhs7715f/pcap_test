a0dbdef90ff75f29b288a33caae297ced6ad9fed
lhs7715f <43023081+lhs7715f@users.noreply.github.com>
2018-09-26 22:17:34 +0900

    Update main.cpp
    추가적인 수정입니다

--- main.cpp
+++ main.cpp
@@ -84,7 +84,7 @@ void tcp_print(u_int8_t *buf, int size)
 {
         struct iphdr* ip = (struct iphdr *)(buf + sizeof(struct ethhdr));
         u_int16_t ip_hdr_len = iphdr->IHL*4;
-struct tcphdr* tcp = (struct tcphdr*)(buf + ip_hdr_len + sizeof(struct ethhdr));
+        struct tcphdr* tcp = (struct tcphdr*)(buf + ip_hdr_len + sizeof(struct ethhdr));
         u_int8_t data_offset =(uint8_t*)(tcp->frag)[0] & 0xF0;
         int header_size = sizeof(struct ethhdr) + ip_hdr_len + 4*data_offset;
 
@@ -99,33 +99,37 @@ struct tcphdr* tcp = (struct tcphdr*)(buf + ip_hdr_len + sizeof(struct ethhdr));
 }
 
 void usage() {
-  printf("syntax: pcap_test <interface>\n");
-  printf("sample: pcap_test wlan0\n");
+        printf("syntax: pcap_test <interface>\n");
+        printf("sample: pcap_test wlan0\n");
 }
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    usage();
-    return -1;
-  }
-char* dev = argv[1];
-  char errbuf[PCAP_ERRBUF_SIZE];
-  pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-  if (handle == NULL) {
-    fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
-    return -1;
-  }
+int main(int argc, char* argv[]) 
+{
+        if (argc != 2) 
+        {
+                usage();
+                return -1;
+        }
+        
+        char* dev = argv[1];
+        char errbuf[PCAP_ERRBUF_SIZE];
+        pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+        if (handle == NULL) 
+        {
+                fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
+                return -1;
+        }
 
-  while (1) {
-    struct pcap_pkthdr* header;
-    const u_char* packet;
-    int res = pcap_next_ex(handle, &header, &packet);
-    if (res == 0) continue;
-    if (res == -1 || res == -2) break;
-    printf("%u bytes captured\n", header->caplen);
-    eth_print(packet, header->caplen);
-  }
-
-  pcap_close(handle);
-  return 0;
+        while (1) 
+        {
+                struct pcap_pkthdr* header;
+                const u_char* packet;
+                int res = pcap_next_ex(handle, &header, &packet);
+                if (res == 0) continue;
+                if (res == -1 || res == -2) break;
+                printf("%u bytes captured\n", header->caplen);
+                eth_print(packet, header->caplen);
+        }
+        pcap_close(handle);
+        return 0;
 }
